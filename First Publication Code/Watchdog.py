b1be0f500a83d080dcb3b1ba24aa062e0db4dd8f
jmb-brinc <justin@brincdrones.com>
2021-10-12 17:22:10 -0500

    Initial Commit
    The initial commit of the code associated with my dissertation research. This update includes the initial commit of code adapted from the initial publication and presentation at the ICAART 2021 Conference.

---
+++ First Publication Code/Watchdog.py
@@ -0,0 +1,182 @@
+import tensorflow as tf
+from tensorflow.keras import activations, datasets, layers, models, utils
+import numpy as np
+from sklearn.metrics import roc_curve
+
+
+def image_rmse(image1, image2):
+    """
+    This function calculates the RMSE value between two images.
+
+    :param image1: First image in the comparison
+    :param image2: Second image in the comparison
+    :return: Root Mean Squared Error
+    """
+    error = np.sqrt(np.sum((image1.astype('float'))
+                           - image2.astype('float')) ** 2)
+    return error
+
+
+def load_training_dataset():
+    """
+    This function loads the training portion of the MNIST dataset. The training dataset
+    is further divided in to training and validation images.
+
+    :return: MNIST training and validation data
+    """
+    # Load the MNIST training subset
+    (x_train, y_train), (_, _) = datasets.mnist.load_data()
+
+    x_train = x_train / 255.0  # Normalize the images
+    y_train = utils.to_categorical(y_train)  # One-hot encode labels
+
+    # Split between training and validation data
+    x_val = x_train[-10000:]
+    x_train = x_train[:-10000]
+    y_val = y_train[-10000:]
+    y_train = y_train[:-10000]
+
+    return x_train, x_val, y_train, y_val
+
+
+def load_evaluation_datasets():
+    """
+    This function creates an in-distribution, out-of-distribution,
+    and a mixed distribution dataset. These are used to evaluate
+    the watchdog functionality.
+
+    :return: The in-distribution, out-of-distribution and mixed
+    distribution evaluation sets.
+    """
+    # Load the MNIST and Fashion MNIST test subsets
+    (_, _), (in_imgs, in_labs) = datasets.mnist.load_data()
+    (_, _), (out_imgs, out_labs) = datasets.fashion_mnist.load_data()
+
+    # Normalize Images
+    in_imgs = in_imgs / 255.0
+    out_imgs = out_imgs / 255.0
+
+    # One-hot encode the labels
+    in_labs = utils.to_categorical(in_labs)
+    out_labs = utils.to_categorical(out_labs)
+
+    # create the mixed distribution_dataset
+    mixed_imgs = np.append(in_imgs, out_imgs, axis=0)
+    mixed_labs = np.append(in_labs, out_labs, axis=0)
+
+    return (in_imgs, in_labs), (out_imgs, out_labs), (mixed_imgs, mixed_labs)
+
+
+def build_classifier(input_shape, filters, ks, dropout, num_labels):
+    """
+    This function builds a CNN using TensorFlow's Sequential API.
+
+    :param input_shape: Input shape for the classifier
+    :param filters: Number of filters in the 1st convolutional layer
+    :param ks: Kernel size of the convolutional layers
+    :param dropout: Dropout
+    :param num_labels: Number of labels in the classifier output
+    :return: The CNN based classifier model
+    """
+
+    classifier = models.Sequential()
+    classifier.add(layers.Conv2D(input_shape=input_shape,
+                                 filters=filters,
+                                 kernel_size=ks,
+                                 activation=activations.relu()
+                                 ))
+    classifier.add(layers.MaxPooling2D((2, 2)))
+
+    classifier.add(layers.Conv2D(filters=filters * 2,
+                                 kernel_size=ks,
+                                 activation=activations.relu()
+                                 ))
+    classifier.add(layers.MaxPooling2D((2, 2)))
+
+    classifier.add(layers.Conv2D(filters=filters * 4,
+                                 kernel_size=ks,
+                                 activation=activations.relu()
+                                 ))
+    classifier.add(layers.MaxPooling2D((2, 2)))
+
+    classifier.add(layers.Flatten)
+    classifier.add(layers.Dropout(dropout))
+    classifier.add(layers.Dense(num_labels,
+                                activation=activations.softmax()
+                                ))
+
+    return classifier
+
+
+def build_autoencoder(input_shape, layer_filters, ks, latent_dim):
+    """
+    This function builds an autoencoder which is used to reconstruct the input data.
+
+    :param input_shape: Shape of the input data
+    :param layer_filters: Array containing number of filters
+    :param ks: Kernel size for the convolutions
+    :param latent_dim: Dimension of the latent space
+    :return:
+    """
+
+    #  Define the encoder layers
+    x = tf.keras.layers.Input(shape=input_shape, name='encoder_input')
+    for filters in layer_filters:
+        x = tf.keras.layers.Conv2D(filters=filters, kernel_size=ks,
+                                   activation='relu', strides=2,
+                                   padding='same')(x)
+    shape = tf.keras.backend.int_shape(x)
+    x = tf.keras.layers.Flatten()(x)
+    latent = tf.keras.layers.Dense(latent_dim, name='latent_vector')(x)
+
+    encoder = tf.keras.Model(input, latent, name='encoder')
+
+    # Build the Decoder
+    latent_input = tf.keras.layers.Input(shape=(latent_dim,),
+                                          name='decoder_input')
+
+    x = tf.keras.layers.Dense(shape[1] * shape[2] * shape[3])(latent_input)
+    x = tf.keras.layers.Reshape((shape[1], shape[2], shape[3]))(x)
+
+    for filters in layer_filters[::-1]:
+        x = tf.keras.layers.Conv2DTranspose(filters=filters,
+                                            kernel_size=ks,
+                                            activation='relu', strides=2,
+                                            padding='same')(x)
+
+    outputs = tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=ks,
+                                              activation='sigmoid', padding='same',
+                                              name='decoder_output')(x)
+
+    decoder = tf.keras.Model(latent_input, outputs, name='decoder')
+
+    autoencoder = tf.keras.Model(encoder, decoder, name = 'autoencoder')
+
+    return autoencoder
+
+
+def train_network(network, epochs, batch_size, train_x, train_y, early_stopping):
+    """
+    This function trains a neural network based on the provided parameters.
+
+    :param network: The network to be trained
+    :param epochs: The number of epochs to be trained
+    :param batch_size: The batch size for training
+    :param train_x: The input data
+    :param train_y: The output data
+    :param early_stopping: Whether to implement early_stopping criteria
+    :return: None
+    """
+
+    if early_stopping:
+        callbacks = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='auto')
+    else:
+        callbacks = None
+
+    network.fit(train_x,
+                train_y,
+                epochs=epochs,
+                batch_size=batch_size,
+                callbacks=[callbacks],
+                )
+
---
+++ First Publication Code/main.py
@@ -0,0 +1,48 @@
+import Watchdog
+
+# Load the datasets
+train_x, train_y = Watchdog.load_training_dataset()
+in_dist_img, in_dist_lab, _, _, mix_dist_img, mix_dist_lab = Watchdog.load_evaluation_datasets()
+
+# Define the network and training hyperparameters
+IMG_SHAPE = train_x.shape[1]
+KERNEL = 2
+LATENT_DIM = 32
+FILTERS = 16
+DROPOUT = 0.15
+NUM_LABELS = 10
+LAYER_FILTERS = [16, 32, 64]
+EPOCHS = 20
+BATCH_SIZE = 64
+E_STOP = True
+
+# Define the networks using the hyperparameters above
+classifier = Watchdog.build_classifier(input_shape=IMG_SHAPE,
+                                       filters=FILTERS,
+                                       ks=KERNEL,
+                                       dropout=DROPOUT,
+                                       num_labels=NUM_LABELS
+                                       )
+
+ae = Watchdog.build_autoencoder(input_shape=IMG_SHAPE,
+                                layer_filters=LAYER_FILTERS,
+                                ks=KERNEL,
+                                latent_dim=LATENT_DIM
+                                )
+
+# Train the networks
+Watchdog.train_network(network=classifier,
+                       epochs=EPOCHS,
+                       batch_size=BATCH_SIZE,
+                       train_x=train_x,
+                       train_y=train_y,
+                       early_stopping=E_STOP
+                       )
+
+Watchdog.train_network(network=ae,
+                       epochs=EPOCHS,
+                       batch_size=BATCH_SIZE,
+                       train_x=train_x,
+                       train_y=train_y,
+                       early_stopping=E_STOP
+                       )
